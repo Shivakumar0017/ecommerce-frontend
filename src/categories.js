3129c6a5f8d80e84e2c03846efe56f9af6ebba4e
rk0777 <coonoorrshivakumar@gmail.com>
2023-02-13 18:28:51 +0530

    First Commit

---
+++ src/categories.js
@@ -0,0 +1,26 @@
+const categories = [
+    {
+        name: "Dress",
+        img: "https://res.cloudinary.com/dojujbi3u/image/upload/v1673183212/fullsizeoutput_6bcd_1_1080x_maizvg.jpg",
+    },
+    {
+        name: "Phones",
+        img: "https://res.cloudinary.com/dojujbi3u/image/upload/v1673183760/photo-1672906674630-67866460f965_wox60v.jpg",
+    },
+    {
+        name: "Ear-Phones",
+        img: "https://res.cloudinary.com/dojujbi3u/image/upload/v1673183626/photo-1668073343175-31e76569eb8f_act359.jpg",
+    },
+    {
+        name: "Shoes",
+        img: "https://res.cloudinary.com/dojujbi3u/image/upload/v1672994917/cld-sample-5.jpg",
+    },
+    {
+        name: "Toys",
+        img: "https://res.cloudinary.com/dojujbi3u/image/upload/v1673243619/photo-1672487065273-212171c80dbe_tfkpcf.jpg",
+    },
+
+    { name: "Laptops", img: "https://images.unsplash.com/photo-1575909812264-6902b55846ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" },
+];
+
+export default categories;
---
+++ src/pages/Signup.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Col, Container, Form ,Button,Row,Alert} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import  { useState } from "react";
+import "./Signup.css";
+import { useSignupMutation } from "../services/appApi";
+
+function Signup() {
+    const[email,setEmail]=useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [signup, { error, isLoading, isError }] = useSignupMutation();
+
+    function handleSignup(e) {
+        e.preventDefault();
+        signup({ name, email, password });
+    }
+
+  return (
+    <Container>
+        <Row>
+            <Col md={6} className="signup__form--container">
+                <Form style={{width:"100%"}} onSubmit={handleSignup}>
+                    <h1>Create your account</h1>
+                    {isError && <Alert variant="danger">{error.data}</Alert>}
+                    <Form.Group>
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" placeholder="Your name" value={name} required onChange={(e) => setName(e.target.value)} />
+                        </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control type='email' placeholder='Enter Email' value={email} required onChange={(e)=>setEmail(e.target.value)}/>
+                    </Form.Group>
+                    <Form.Group className='mb-3'>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type='password' placeholder='Enter Password' value={password}  required onChange={(e)=>setPassword(e.target.value)}/>
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Button type="Submit" disabled={isLoading}>Sign Up</Button>
+                    </Form.Group>
+                    <p>Already have an account? <Link to="/Login">Login</Link>{""}</p>
+                </Form>
+
+            </Col>
+            <Col md={6} className="signup__image--container"></Col>
+        </Row>
+    </Container>
+  )
+}
+
+export default Signup
